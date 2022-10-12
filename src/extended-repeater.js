eef6881d7e06b9b33dba066d9f99f9de07a3a518
Alexander Megadrive <alexmihailov91@gmail.com>
2022-10-12 16:35:23 +0300

    feat: add extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+console.log('options :', options);
+let array = []
+let stringFormat = String(str);
+let repeatQuantity = options.repeatTimes || 1
+// console.log('repeatQuantity :', repeatQuantity);
+let repeatQuantityAdditional = options.additionRepeatTimes ? options.repeatTimes : 1
+let finalSeparator = options.separator ? options.separator : '+'
+
+
+
+  function generateString(string, times, addition, separator) {
+    let result = '';
+    times = times ? times : 1
+    addition = addition !== undefined ? String(addition) : '';
+    separator = separator ? separator : '|';
+    for (let i=0; i<times; i++) {
+      result = result ? result+separator+addition : string+addition
+    } 
+    return result
+  }
+
+let substr = generateString(stringFormat,options.additionRepeatTimes, options.addition,options.additionSeparator)
+for (let i=0; i<repeatQuantity; i++) {
+array.push(substr)
 }
+return array.join(finalSeparator)
+}
+
+
+
+console.log(repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }))
+
 
 module.exports = {
   repeater
