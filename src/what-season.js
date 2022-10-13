0ce15435cd593f40c28e5c417e1105c26e4febf3
Alexander Megadrive <alexmihailov91@gmail.com>
2022-10-13 21:39:09 +0300

    feat: add what season solution

--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+
+  if (!date) return 'Unable to determine the time of year!';
+  if (!(date instanceof Date))  throw Error('Invalid date!')
+  if (Object.getOwnPropertyNames(date).length > 0) throw Error('Invalid date!')
+  // for (i=0; i<date.length; i++) {
+  // }
+  let monthNumber = date.getMonth()
+
+  if (monthNumber < 2 || monthNumber==12 || monthNumber==11) return 'winter';
+  else if (monthNumber<5) return 'spring';
+  else if (monthNumber<8) return 'summer';
+  else if (monthNumber<11) return 'autumn';
+  
+
 }
+// console.log(getSeason(1994, 8, 26, 3, 0, 11, 500))
+// console.log(new Date(2019, 11, 22, 23, 45, 11, 500))
 
 module.exports = {
   getSeason
